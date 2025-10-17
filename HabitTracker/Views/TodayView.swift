6ddf062fd89ab59ca6ecbbfdc1374b76405ecc0b
yashpatil04 <yash.s.patil@student.uts.edu.au>
2025-10-17 23:05:17 +1100

    Enhance TodayView with habit deletion functionality (#2)
    Implemented habit deletion and updated UI structure.

--- HabitTracker/Views/TodayView.swift
+++ HabitTracker/Views/TodayView.swift
@@ -7,12 +7,34 @@
 
 import SwiftUI
 
-struct TodayView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+import SwiftUICore
 
-#Preview {
-    TodayView()
+struct TodayView: View {
+    @Environment(\.managedObjectContext) private var ctx
+    @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \Habit.createdAt, ascending: true)],
+                  predicate: NSPredicate(format: "isArchived == NO"))
+    private var habits: FetchedResults<Habit>
+
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(habits) { habit in
+                    HabitRow(habit: habit)
+                }.onDelete(perform: deleteHabit)
+            }
+            .navigationTitle("Today")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink(destination: HabitEditView()) {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                }
+            }
+        }
+    }
+
+    private func deleteHabit(at offsets: IndexSet) {
+        offsets.map { habits[$0] }.forEach(ctx.delete)
+        try? ctx.save()
+    }
 }
