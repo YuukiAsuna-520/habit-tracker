356aa39cde58500499d441264217ceedca2c49fe
yashpatil04 <yash.s.patil@student.uts.edu.au>
2025-10-19 17:03:26 +1100

    Habit

--- HabitTracker/Models/Habit.swift
+++ HabitTracker/Models/Habit.swift
@@ -2,7 +2,30 @@
 //  Habit.swift
 //  HabitTracker
 //
-//  Created by 黑白熊 on 15/10/2025.
+//  Created by Yash Patil on 17/10/2025.
 //
 
 import Foundation
+
+struct Habit: Identifiable, Hashable {
+    let id: UUID
+    var title: String
+    var targetPerDay: Double
+    var progress: Double
+    var reminderHour: Int
+    var createdAt: Date
+
+    init(id: UUID = UUID(),
+         title: String,
+         targetPerDay: Double = 1,
+         progress: Double = 0,
+         reminderHour: Int = 8,
+         createdAt: Date = Date()) {
+        self.id = id
+        self.title = title
+        self.targetPerDay = targetPerDay
+        self.progress = progress
+        self.reminderHour = reminderHour
+        self.createdAt = createdAt
+    }
+}
